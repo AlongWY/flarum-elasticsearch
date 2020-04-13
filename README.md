2712ae72069b2768631688f9c8ed947b5710c0e1
Feng Yunlong <ylfeng@ir.hit.edu.cn>
2020-04-13 16:58:25 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-# flarum-elasticsearch
+# Flarum 论坛中文搜索插件
 Flarum 论坛中文搜索插件 - 基于 elasticsearch 搜索引擎开发
 
 ### 安装插件
 `composer require alongwy/flarum-elasticsearch`
 > 如果遇到权限问题， 则使用 `sudo` 进行安装, 安装完成后对相关文件权限进行设置
 
-### 其他问题
-Flarum 所有文件权限最好是 apache 或 nginx 有权限的用户才行，例如 *www-data* 用户，如果有各种权限问题，就把 flarum 的文件设置为相关的用户和用户组即可  
-
 ``
 sudo chown -R www-data:www-data flarum/
 ``
+### 更新
+
+```sh
+composer update alongwy/flarum-elasticsearch
+```
+
+### Links
+
+- [Packagist](https://packagist.org/packages/alongwy/flarum-elasticsearch)
+
+
+### 其他问题
+Flarum 所有文件权限最好是 apache 或 nginx 有权限的用户才行，例如 *www-data* 用户，如果有各种权限问题，就把 flarum 的文件设置为相关的用户和用户组即可  
--- extend.php
+++ extend.php
@@ -1,9 +1,19 @@
 <?php
 
-namespace Plugin\ESearch;
+/*
+ * This file is part of alongwy/flarum-elasticsearch.
+ *
+ * Copyright (c) 2020 alongwy.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Alongwy\FlarumElasticsearch;
 
 use Flarum\Extend;
 use Illuminate\Contracts\Events\Dispatcher;
+use Plugin\ESearch\SearchDispatcher;
 use Plugin\ESearch\Utils\LoadSettings;
 use Plugin\ESearch\Controller\SearchController;
 
@@ -11,8 +21,9 @@ return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__ . '/js/dist/forum.js'),
     (new Extend\Frontend('admin'))
-        ->js(__DIR__ . '/js/dist/admin.js'),
-    (new Extend\Locales(__DIR__ . '/locale')),
+        ->js(__DIR__ . '/js/dist/admin.js')
+        ->css(__DIR__ . '/resources/less/admin.less'),
+    new Extend\Locales(__DIR__ . '/resources/locale'),
     (new Extend\Routes('api'))
         ->get('/es/discussions', 'es.discussions.index', SearchController::class),
     function (Dispatcher $events) {
